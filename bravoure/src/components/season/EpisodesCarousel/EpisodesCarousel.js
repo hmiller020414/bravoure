0c73e2ef1ba36b609e87525fbb5b4920e4757b7b
hmiller020414 <hayleymm14@gmail.com>
2022-01-24 11:03:23 +0100

    carousel js functionality

--- bravoure/src/components/season/EpisodesCarousel/EpisodesCarousel.js
+++ bravoure/src/components/season/EpisodesCarousel/EpisodesCarousel.js
@@ -1,5 +1,9 @@
 import CarouselSlide from "../CarouselSlide/CarouselSlide"
 import '../season.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { useState, useEffect } from 'react'
 
 const EpisodesCarousel = ({season}) => {
 
@@ -10,9 +14,68 @@ const EpisodesCarousel = ({season}) => {
             )
         : null;
 
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [length, setLength] = useState()
+    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
+    const [nextBtnDisabled, setNextBtnDisabled] = useState(false)
+
+    useEffect(() => {
+        setLength((seasonEpisodes?.length * 200) + ((seasonEpisodes?.length - 1) * 28))
+    }, [seasonEpisodes])
+
+    useEffect(() => {
+        checkIdx()
+    }, [currentIndex])
+
+    const next = () => {
+        if (currentIndex < (length - 1)) {
+            setCurrentIndex(prevState => prevState + 1)
+        }
+    }
+    
+    const prev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(prevState => prevState - 1)
+        }
+    }
+
+    const checkIdx =() => {
+        if (currentIndex == 0) {
+            setPrevBtnDisabled(true)
+            console.log("here")
+        } else if (currentIndex == seasonEpisodes?.length - 1) {
+            setNextBtnDisabled(true)
+        } else {
+            console.log("something")
+            setPrevBtnDisabled(false)
+            setNextBtnDisabled(false)
+        }
+    }
+
     return (
         <div className="EpisodesCarousel">
-            {slides}
+            <div 
+                className="EpisodesCarousel-Inner"
+                style={{ transform: `translateX(-${currentIndex * 228}px)` }}
+            >
+                {slides}
+            </div>
+            <div className="EpisodesCarousel-Btns">
+                <button 
+                    className="EpisodesCarousel-Btn Prev"
+                    onClick={prev}
+                    disabled={prevBtnDisabled}
+                >
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                </button>
+                <button 
+                    className="EpisodesCarousel-Btn Next"
+                    onClick={next}
+                    disabled={nextBtnDisabled}
+                >
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </button>
+            </div>
         </div>
     )
 }
