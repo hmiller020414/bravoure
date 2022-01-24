f7a4f924cf5e20fbe48734c34569f091c462993e
hmiller020414 <hayleymm14@gmail.com>
2022-01-24 09:37:57 +0100

    add star to episode rating

--- bravoure/src/components/episode/EpisodeRating/EpisodeRating.js
+++ bravoure/src/components/episode/EpisodeRating/EpisodeRating.js
@@ -1,9 +1,12 @@
 import '../episode.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const EpisodeRating = ({rating}) => {
 
     return (
         <div className="EpisodeRating">
+            <FontAwesomeIcon icon={faStar} className="EpisodeRating-Star" />
             <p><b>{rating}</b>/10</p>
         </div>
     )
